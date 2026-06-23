d268afb76d6afb9abfb8beb89e601949e7d1fdff
logann-grange <logann.grange@laplateforme.io>
2026-06-23 13:49:49 +0200

    test connexion multiplateforme

--- client/logic/src/client.c
+++ client/logic/src/client.c
@@ -1,34 +1,66 @@
 #include <stdio.h>
-#include <winsock2.h>
-#include <ws2tcpip.h>
-#pragma comment(lib, "ws2_32.lib")
+#include <string.h>
 
-int main() {
-    WSADATA wsaData;
+#ifdef _WIN32
+    #include <winsock2.h>
+    #include <ws2tcpip.h>
+    #pragma comment(lib, "ws2_32.lib")
+#else
+    #include <sys/socket.h>
+    #include <netinet/in.h>
+    #include <arpa/inet.h>
+    #include <unistd.h>
+    #define SOCKET int
+    #define INVALID_SOCKET (-1)
+    #define SOCKET_ERROR (-1)
+    #define closesocket close
+#endif
+
+int main(int argc, char *argv[]) {
     SOCKET sock;
     struct sockaddr_in serverAddr;
     char buffer[1024];
+    const char *serverIP = "10.10.6.228";
 
+    /* Permet de passer l'IP en argument : ./client 192.168.1.10 */
+    if (argc > 1) {
+        serverIP = argv[1];
+    }
+
+#ifdef _WIN32
+    WSADATA wsaData;
     if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
         printf("Erreur WSAStartup\n");
         return 1;
     }
+#endif
 
     sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
     if (sock == INVALID_SOCKET) {
-        printf("Erreur socket: %d\n", WSAGetLastError());
+        printf("Erreur socket\n");
+#ifdef _WIN32
         WSACleanup();
+#endif
         return 1;
     }
 
     serverAddr.sin_family = AF_INET;
     serverAddr.sin_port = htons(8080);
-    inet_pton(AF_INET, "10.10.6.228", &serverAddr.sin_addr);
+    if (inet_pton(AF_INET, serverIP, &serverAddr.sin_addr) != 1) {
+        printf("Adresse IP invalide: %s\n", serverIP);
+        closesocket(sock);
+#ifdef _WIN32
+        WSACleanup();
+#endif
+        return 1;
+    }
 
     if (connect(sock, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR) {
-        printf("Erreur connexion: %d\n", WSAGetLastError());
+        printf("Erreur connexion\n");
         closesocket(sock);
+#ifdef _WIN32
         WSACleanup();
+#endif
         return 1;
     }
 
@@ -42,6 +74,8 @@ int main() {
     }
 
     closesocket(sock);
+#ifdef _WIN32
     WSACleanup();
+#endif
     return 0;
 }

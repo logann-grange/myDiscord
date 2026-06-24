0efb32c0d816498c1ff00c370770c659d40073c5
logann-grange <logann.grange@laplateforme.io>
2026-06-24 12:01:31 +0200

    envoi et reception des messages silultanée

--- client/logic/src/client.c
+++ client/logic/src/client.c
@@ -4,26 +4,60 @@
 #ifdef _WIN32
     #include <winsock2.h>
     #include <ws2tcpip.h>
+    #include <windows.h>
     #pragma comment(lib, "ws2_32.lib")
+    #define THREAD_RET DWORD WINAPI
 #else
     #include <sys/socket.h>
     #include <netinet/in.h>
     #include <arpa/inet.h>
     #include <unistd.h>
+    #include <pthread.h>
     #define SOCKET int
     #define INVALID_SOCKET (-1)
     #define SOCKET_ERROR (-1)
     #define closesocket close
+    #define THREAD_RET void*
 #endif
 
-int main(int argc, char *argv[]) {
-    SOCKET sock;
-    struct sockaddr_in serverAddr;
+SOCKET sock;
+volatile int running = 1;
+
+// Thread dedie a la reception :
+THREAD_RET receiveMessages(void *arg) {
     char buffer[1024];
+    int bytesReceived;
+
+    while (running) {
+        bytesReceived = recv(sock, buffer, sizeof(buffer) - 1, 0);
+        if (bytesReceived <= 0) {
+            if (running) {
+                printf("\nServeur deconnecte.\n");
+            }
+            running = 0;
+            break;
+        }
+        buffer[bytesReceived] = '\0';
+        printf("\nServeur: %s\nVous: ", buffer);
+        fflush(stdout);
+
+        if (strcmp(buffer, "/quit") == 0) {
+            running = 0;
+            break;
+        }
+    }
+#ifdef _WIN32
+    return 0;
+#else
+    return NULL;
+#endif
+}
+
+int main(int argc, char *argv[]) {
+    struct sockaddr_in serverAddr;
     char message[1024];
     const char *serverIP = "10.10.6.228";
 
-    //Permet de passer l'IP en argument :
     if (argc > 1) {
         serverIP = argv[1];
     }
@@ -67,34 +101,59 @@ int main(int argc, char *argv[]) {
 
     printf("Connecte au serveur %s:8080. Tapez /quit pour fermer la conversation.\n", serverIP);
 
-    while (1) {
-        printf("Vous: ");
-        fflush(stdout);
+    // Lancement du thread de reception en arriere-plan
+#ifdef _WIN32
+    HANDLE recvThread = CreateThread(NULL, 0, receiveMessages, NULL, 0, NULL);
+    if (recvThread == NULL) {
+        printf("Erreur creation thread\n");
+        closesocket(sock);
+        WSACleanup();
+        return 1;
+    }
+#else
+    pthread_t recvThread;
+    if (pthread_create(&recvThread, NULL, receiveMessages, NULL) != 0) {
+        printf("Erreur creation thread\n");
+        closesocket(sock);
+        return 1;
+    }
+#endif
+
+    // Le thread principal s'occupe de l'envoi
+    printf("Vous: ");
+    fflush(stdout);
+    while (running) {
         if (fgets(message, sizeof(message), stdin) == NULL) {
+            running = 0;
             break;
         }
         message[strcspn(message, "\n")] = '\0';
 
-        send(sock, message, (int)strlen(message), 0);
+        if (send(sock, message, (int)strlen(message), 0) == SOCKET_ERROR) {
+            running = 0;
+            break;
+        }
 
         if (strcmp(message, "/quit") == 0) {
+            running = 0;
             break;
         }
 
-        int bytesReceived = recv(sock, buffer, sizeof(buffer) - 1, 0);
-        if (bytesReceived <= 0) {
-            printf("Serveur deconnecte.\n");
-            break;
-        }
-        buffer[bytesReceived] = '\0';
-        printf("Serveur: %s\n", buffer);
-
-        if (strcmp(buffer, "/quit") == 0) {
-            break;
+        if (running) {
+            printf("Vous: ");
+            fflush(stdout);
         }
     }
 
-    closesocket(sock);
+    closesocket(sock); // debloque le thread de reception s'il attend un recv()
+
+#ifdef _WIN32
+    WaitForSingleObject(recvThread, 2000);
+    CloseHandle(recvThread);
+#else
+    pthread_join(recvThread, NULL);
+#endif
+
 #ifdef _WIN32
     WSACleanup();
 #endif
--- server/src/server.c
+++ server/src/server.c
@@ -4,24 +4,60 @@
 #ifdef _WIN32
     #include <winsock2.h>
     #include <ws2tcpip.h>
+    #include <windows.h>
     #pragma comment(lib, "ws2_32.lib")
     typedef int socklen_t;
+    #define THREAD_RET DWORD WINAPI
 #else
     #include <sys/socket.h>
     #include <netinet/in.h>
     #include <arpa/inet.h>
     #include <unistd.h>
+    #include <pthread.h>
     #define SOCKET int
     #define INVALID_SOCKET (-1)
     #define SOCKET_ERROR (-1)
     #define closesocket close
+    #define THREAD_RET void*
 #endif
 
+SOCKET clientSocket;
+volatile int running = 1;
+
+// Thread dedie a la reception :
+THREAD_RET receiveMessages(void *arg) {
+    char buffer[1024];
+    int bytesReceived;
+
+    while (running) {
+        bytesReceived = recv(clientSocket, buffer, sizeof(buffer) - 1, 0);
+        if (bytesReceived <= 0) {
+            if (running) {
+                printf("\nClient deconnecte.\n");
+            }
+            running = 0;
+            break;
+        }
+        buffer[bytesReceived] = '\0';
+        printf("\nClient: %s\nVous: ", buffer);
+        fflush(stdout);
+
+        if (strcmp(buffer, "/quit") == 0) {
+            running = 0;
+            break;
+        }
+    }
+#ifdef _WIN32
+    return 0;
+#else
+    return NULL;
+#endif
+}
+
 int main() {
-    SOCKET listenSocket, clientSocket;
+    SOCKET listenSocket;
     struct sockaddr_in serverAddr, clientAddr;
     socklen_t clientAddrSize = sizeof(clientAddr);
-    char buffer[1024];
     char message[1024];
 
 #ifdef _WIN32
@@ -76,36 +112,61 @@ int main() {
 
     printf("Client connecte. Tapez /quit pour fermer la conversation.\n");
 
-    while (1) {
-        /* Reception du message du client */
-        int bytesReceived = recv(clientSocket, buffer, sizeof(buffer) - 1, 0);
-        if (bytesReceived <= 0) {
-            printf("Client deconnecte.\n");
-            break;
-        }
-        buffer[bytesReceived] = '\0';
-        printf("Client: %s\n", buffer);
+    // Lancement du thread de reception en arriere-plan
+#ifdef _WIN32
+    HANDLE recvThread = CreateThread(NULL, 0, receiveMessages, NULL, 0, NULL);
+    if (recvThread == NULL) {
+        printf("Erreur creation thread\n");
+        closesocket(clientSocket);
+        closesocket(listenSocket);
+        WSACleanup();
+        return 1;
+    }
+#else
+    pthread_t recvThread;
+    if (pthread_create(&recvThread, NULL, receiveMessages, NULL) != 0) {
+        printf("Erreur creation thread\n");
+        closesocket(clientSocket);
+        closesocket(listenSocket);
+        return 1;
+    }
+#endif
 
-        if (strcmp(buffer, "/quit") == 0) {
-            break;
-        }
-
-        /* Envoi d'une reponse */
-        printf("Vous: ");
-        fflush(stdout);
+    // thread principal s'occupe de l'envoi
+    printf("Vous: ");
+    fflush(stdout);
+    while (running) {
         if (fgets(message, sizeof(message), stdin) == NULL) {
+            running = 0;
             break;
         }
         message[strcspn(message, "\n")] = '\0';
 
-        send(clientSocket, message, (int)strlen(message), 0);
+        if (send(clientSocket, message, (int)strlen(message), 0) == SOCKET_ERROR) {
+            running = 0;
+            break;
+        }
 
         if (strcmp(message, "/quit") == 0) {
+            running = 0;
             break;
+        }
+
+        if (running) {
+            printf("Vous: ");
+            fflush(stdout);
         }
     }
 
-    closesocket(clientSocket);
+    closesocket(clientSocket); // debloque le thread de reception s'il attend un recv()
+
+#ifdef _WIN32
+    WaitForSingleObject(recvThread, 2000);
+    CloseHandle(recvThread);
+#else
+    pthread_join(recvThread, NULL);
+#endif
+
     closesocket(listenSocket);
 
 #ifdef _WIN32
